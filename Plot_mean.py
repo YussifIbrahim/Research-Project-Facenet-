b6f25d5ffc02ca81238a3c79b8dfc1bf06f68e41
Yussif Ibrahim <59871375+YussifIbrahim@users.noreply.github.com>
2020-08-14 17:32:45 +0200

    Added comments to code

--- Plot_mean.py
+++ Plot_mean.py
@@ -13,17 +13,24 @@ import math
 from my_triple_loss import load_raw_data,form_test_data,compute_dist,compute_probs
 
 
-def remove_outliers(arr):
-    elements = np.array(arr)
 
-    mean = np.mean(elements, axis=0)
-    sd = np.std(elements, axis=0)
-    
-    final_list = [x for x in arr if (x > mean - 2 * sd)]
-    final_list = [x for x in final_list if (x < mean + 2 * sd)]
-    
-    return final_list
 def plot_mean(val, Y_val, model, show=True):
+
+	'''
+	plot the mean of each class 
+
+	arguments:
+	val--the validation set to be plotted
+	Y_val--the labels of the validation set
+	model--the trained model to me used for creating the embeddings
+	
+	returns:
+	plot--a plot of the mean
+	or
+	dictionary--a dictionary of the mean of each class using class as key. 
+	embeddings--embeddings is the embeddings obtained from predictions
+	'''
+	#create embeddings
     embeddings = model.predict(val)
     c =plt.subplot()
     
@@ -36,19 +43,28 @@ def plot_mean(val, Y_val, model, show=True):
     
     col= ["gold","indigo","yellow","blue","orange","pink","chartreuse","brown","green","gray","black","purple","cyan","magenta","olive","red","aqua"]
     
+	#form a dictionary to store the mean of each class
     dictionary ={}
+	#iterate through each class
     for x in range(17):
+	
+		#store all samples per class x in variable l
         l=[i for i in range(len(Y_val)) if Y_val[i]==x]
+        
+		#variable to store the angles
         radians =[]
         for y in l:
+			#find the x and y axes of the plots
             embed_1d=embeddings[y,1]
             embed_2d=embeddings[y,0]
             
+            #find the angle per sample
             angles = math.atan(embed_2d/embed_1d)
             if (embed_1d < 0 and embed_2d> 0) or (embed_1d < 0 and embed_2d< 0):
                 angles = angles + math.radians(180)
             radians.append(angles)
-        #print(radians)
+            
+        #find the mean and standard 
         mean_angle= np.mean(radians)
         sdv = np.std(radians)
         
@@ -61,17 +77,22 @@ def plot_mean(val, Y_val, model, show=True):
         b=1
         degrees=math.degrees(mean_angle)
         
+        #calculate the x and y axes of the mean of each class
         new_x = (a*b)/(math.sqrt((b**2)+(a**2)*(math.tan(mean_angle))**2))
         new_y = (a*b*(math.tan(mean_angle)))/(math.sqrt((b**2)+(a**2)*(math.tan(mean_angle))**2))
         
+        #make transformations to the x and y values based on the angle
         if 0<=degrees<90:
             new_x = new_x
             new_y = new_y
         elif 90<degrees<270:
             new_x = -new_x
             new_y = -new_y
-        
+            
+        #add the mean to the dictionary using the class as key
         dictionary[x] = [float(new_x), float(new_y)]
+        
+		#plot the mean 
         c.scatter(new_x,new_y,color=col[x], label=x,s=100,alpha=0.6)
        
     c.grid(True)
@@ -83,13 +104,32 @@ def plot_mean(val, Y_val, model, show=True):
     if show==True:
         plt.show()
     elif show == False:
-        return dictionary
+        return dictionary,embeddings
 
 def find_class(train,Y_train,model, val, Y_val):
-    dictionary = plot_mean(train,Y_train,model,show=False)
-    val_embeddings = model.predict(val)
-    
+
+	'''
+	finds the accuracy of the trained model. this is done by comparing the distance between the validation samples and the mean of the 
+	trained set	calculated by plot_mean function above. The new class belongs to the class with the smallest distance between them
+	 arguments:
+	 train--the train set used for training the model
+	 Y_train--the labels of the train set
+	 model--the trained model to be used for prediction
+	 val--the dataset used to test the accuracy
+	 Y_val--the class labels of the test set
+	 
+	 return:
+	 classes--the class with the smallest distance to the sample
+	 Y_val--the true class of the sample
+	 
+	 
+	 
+	'''
+    dictionary ,val_embeddings= plot_mean(train,Y_train,model,show=False)
+    #a list to store the class with smallest distance 
     classes =[]
+    
+	# a dictionary to store the distance for each sample
     dist = {}
     for x in range(len(Y_val)):
         dist.clear()
@@ -98,24 +138,21 @@ def find_class(train,Y_train,model, val, Y_val):
             if dictionary.get(y+1) is None:
                 continue
             
-           # print(dictionary.get(y+1))
-           
-            
-            
+        
             #type_float =[float(i) for i in dictionary.get(y+1)]
             distance = compute_dist(dictionary.get(y+1), val_embeddings[x] )
             dist[y+1] = distance
             
         match = min(dist, key=dist.get)
         classes.append(match)
-#    
-    
-    
-    
-    
+
     return classes,Y_val
 
 def accuracy(classes,Y_val):
+
+	'''
+	calculates the accuracy of results from  find_class() function above
+	'''
     true = 0
     for x in range(len(classes)):
         if classes[x]==Y_val[x]:
@@ -126,15 +163,14 @@ def accuracy(classes,Y_val):
             
 
 if __name__== "__main__":
+	#loads the train and test data with corresponding labels
     _,val,_,Y_val,Y_train_original,_= load_raw_data(train_size=0)
-    train,_,Y_train,_,Y_train_original,_= load_raw_data(train_size=451)
-    model = keras.models.load_model('network.h5')
-    #embeddings = network.predict(train)
-    #dictionary = plot_mean(train, Y_train, model,show=True)
+	#loads trained model
+     model = keras.models.load_model('best.h5')
+    dictionary,embeddings = plot_mean(train, Y_train, model,show=True)
     classes,Y_value = find_class(train,Y_train,model, train,Y_train)
     accuracy(classes,Y_val)
-#    print(dictionary )
-    print(Y_value)
+
     
     
     
